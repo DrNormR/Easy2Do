b8985c3520f9c65ea4bc14d17099b69d27e15bd7
DrNormR <normanr@newmanu.edu>
2026-02-12 18:43:55 -0600

    syncing across systems now working beautifully

--- Easy2Do/Views/NoteWindow.axaml.cs
+++ Easy2Do/Views/NoteWindow.axaml.cs
@@ -7,16 +7,36 @@ using Easy2Do.ViewModels;
 using Easy2Do.Models;
 using Avalonia.VisualTree;
 using System;
+using System.Collections.Generic;
 
 namespace Easy2Do.Views;
 
 public partial class NoteWindow : Window
 {
+    // Static registry for open note windows
+    private static readonly Dictionary<Guid, List<NoteWindow>> OpenWindows = new();
+    // Note ID for this window
+    private Guid? _noteId;
+
     public NoteWindow()
     {
         InitializeComponent();
         Opened += OnWindowOpened;
         Closing += OnWindowClosing;
+        // Subscribe to external note file changes
+        Easy2Do.App.StorageService.NoteFileChanged += OnExternalNoteChanged;
+    }
+
+    // Automatic refresh on external note file change
+    private void OnExternalNoteChanged(Guid id)
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            if (_noteId.HasValue && _noteId.Value == id && DataContext is NoteViewModel vm)
+            {
+                vm.RefreshNoteCommand.Execute(null);
+            }
+        });
     }
 
     private void OnWindowOpened(object? sender, EventArgs e)
@@ -24,6 +44,12 @@ public partial class NoteWindow : Window
         if (DataContext is NoteViewModel vm)
         {
             var note = vm.Note;
+            _noteId = note.Id;
+            // Register this window
+            if (!OpenWindows.TryGetValue(note.Id, out var list))
+                OpenWindows[note.Id] = list = new List<NoteWindow>();
+            if (!list.Contains(this))
+                list.Add(this);
 
             if (!double.IsNaN(note.WindowWidth) && note.WindowWidth > 0)
                 Width = note.WindowWidth;
@@ -39,6 +65,12 @@ public partial class NoteWindow : Window
 
     private void OnWindowClosing(object? sender, WindowClosingEventArgs e)
     {
+        if (_noteId.HasValue && OpenWindows.TryGetValue(_noteId.Value, out var list))
+        {
+            list.Remove(this);
+            if (list.Count == 0)
+                OpenWindows.Remove(_noteId.Value);
+        }
         if (DataContext is NoteViewModel vm)
         {
             var note = vm.Note;
@@ -48,6 +80,7 @@ public partial class NoteWindow : Window
             note.WindowHeight = Height;
             note.ModifiedDate = DateTime.Now;
         }
+
     }
 
     private void OnTextBoxKeyDown(object? sender, KeyEventArgs e)
@@ -295,7 +328,7 @@ private async void OnDueDateButtonClick(object? sender, RoutedEventArgs e)
     }
 }
 
-private void HideAllDropIndicators()
+    private void HideAllDropIndicators()
     {
         foreach (var desc in this.GetVisualDescendants())
         {
